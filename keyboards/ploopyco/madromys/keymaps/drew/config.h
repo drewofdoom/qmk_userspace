4f4cef5683b9ebe7b642e4d365391aaf3ba42cae
Drew DeVore <drew@devorcula.com>
2025-05-04 15:19:14 -0600

    updates

--- keyboards/ploopyco/madromys/keymaps/drew/config.h
+++ keyboards/ploopyco/madromys/keymaps/drew/config.h
@@ -3,7 +3,6 @@
 // Checkout https://github.com/qmk/qmk_firmware/tree/master/keyboards/ploopyco#drag-scroll
 #define PLOOPY_DRAGSCROLL_MOMENTARY
 #define PLOOPY_DRAGSCROLL_INVERT
-#define PLOOPY_DPI_OPTIONS { 800, 1600, 3200, 6400 }
-#define PLOOPY_DPI_DEFAULT 1
-#define PLOOPY_DRAGSCROLL_DIVISOR_H 10.0
-#define PLOOPY_DRAGSCROLL_DIVISOR_V 10.0
+#define PLOOPY_DRAGSCROLL_DIVISOR_H 8.0
+#define PLOOPY_DRAGSCROLL_DIVISOR_V 8.0
+#define DEBOUNCE 10
